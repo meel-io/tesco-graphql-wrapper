dc9804e100ac497c8b34b11a4479861f8bcd0147
e1000-io <emiliodelatorrea@gmail.com>
2018-04-07 22:39:34 +0100

    Fix promise loop¨

--- src/schema.js
+++ src/schema.js
@@ -37,25 +37,31 @@ const resolvers = {
     meals: (root, args, context) => {
       return Promise.all(
         dailyMeals.map(({ products, name, id }) => {
-          return products.map(async ({ name, id }) => {
+          const formattedProducts = products.map(async ({ name, id }) => {
             const data = await fetch(
               `${TESCO_API_URL}?query=${name}&offset=${offset}&limit=${limit}`,
               { headers: { 'Ocp-Apim-Subscription-Key': TESCO_API_KEY } }
             )
-            const response = data.json()
+            const response = await data.json()
             if (response.statusCode) {
               throw response.message
             }
 
             const results = response.uk.ghs.products.results
-            const articles = results.map(
-              ({ id, tpnb, name, image, description }) => {
-                return { id, tpnb, name, image, description }
+            const articles = results.map(result => {
+              return {
+                id: result.id,
+                tpnb: result.tpnb,
+                name: result.name,
+                image: result.image,
+                description: result.description
               }
-            )
+            })
 
             return { name, id, articles }
           })
+
+          return { id, name, products: formattedProducts }
         })
       )
     }
